f0b14a4d595209d26806109f8a34cbf6704556e7
Oliver Calder <oacalder@gmail.com>
2020-05-03 02:22:43 -0500

    Fixed problems with variable resolution in source-pi.sh and corrected output behavior.

--- source-pi.sh
+++ source-pi.sh
@@ -3,14 +3,14 @@
 PROFILECONF=$HOME/.profile
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-grep '$HOME/coding/scripts/common' $PROFILECONF || { 
+grep "$DIR/common" $PROFILECONF > /dev/null || { 
     echo "" >> $PROFILECONF
     echo "if [ -d "$DIR/common" ] ; then" >> $PROFILECONF
     echo "    PATH=$DIR/common:"'$PATH' >> $PROFILECONF
     echo "fi" >> $PROFILECONF
 }
 
-grep '$HOME/coding/scripts/pi' $PROFILECONF || { 
+grep '$HOME/coding/scripts/pi' $PROFILECONF > /dev/null || { 
     echo "" >> $PROFILECONF
     echo "if [ -d "$DIR/pi" ] ; then" >> $PROFILECONF
     echo "    PATH=$DIR/pi:"'$PATH' >> $PROFILECONF
--- source-station.sh
+++ source-station.sh
@@ -2,35 +2,35 @@
 
 PROFILECONF=$HOME/.profile
 
-grep '$HOME/coding/scripts/common' $PROFILECONF || { 
+grep '$HOME/coding/scripts/common' $PROFILECONF > /dev/null || { 
     echo "" >> $PROFILECONF
     echo 'if [ -d "$HOME/coding/scripts/common" ] ; then' >> $PROFILECONF
     echo '    PATH="$HOME/coding/scripts/common:$PATH' >> $PROFILECONF
     echo 'fi' >> $PROFILECONF
 }
 
-grep '$HOME/coding/scripts/backup' $PROFILECONF || { 
+grep '$HOME/coding/scripts/backup' $PROFILECONF > /dev/null || { 
     echo "" >> $PROFILECONF
     echo 'if [ -d "$HOME/coding/scripts/backup" ] ; then' >> $PROFILECONF
     echo '    PATH="$HOME/coding/scripts/backup:$PATH' >> $PROFILECONF
     echo 'fi' >> $PROFILECONF
 }
 
-grep '$HOME/coding/scripts/remote' $PROFILECONF || { 
+grep '$HOME/coding/scripts/remote' $PROFILECONF > /dev/null || { 
     echo "" >> $PROFILECONF
     echo 'if [ -d "$HOME/coding/scripts/remote" ] ; then' >> $PROFILECONF
     echo '    PATH="$HOME/coding/scripts/remote:$PATH' >> $PROFILECONF
     echo 'fi' >> $PROFILECONF
 }
 
-grep '$HOME/coding/scripts/pop' $PROFILECONF || { 
+grep '$HOME/coding/scripts/pop' $PROFILECONF > /dev/null || { 
     echo "" >> $PROFILECONF
     echo 'if [ -d "$HOME/coding/scripts/pop" ] ; then' >> $PROFILECONF
     echo '    PATH="$HOME/coding/scripts/pop:$PATH' >> $PROFILECONF
     echo 'fi' >> $PROFILECONF
 }
 
-grep '$HOME/coding/scripts/station' $PROFILECONF || { 
+grep '$HOME/coding/scripts/station' $PROFILECONF > /dev/null || { 
     echo "" >> $PROFILECONF
     echo 'if [ -d "$HOME/coding/scripts/station" ] ; then' >> $PROFILECONF
     echo '    PATH="$HOME/coding/scripts/station:$PATH' >> $PROFILECONF
