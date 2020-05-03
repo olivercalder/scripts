eadc282f06def06902d66880704c694ca9591095
Oliver Calder <oacalder@gmail.com>
2020-05-03 02:02:30 -0500

    Fixed DIR resolution in path modification additions to .profile.

--- source-pi.sh
+++ source-pi.sh
@@ -5,14 +5,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 grep '$HOME/coding/scripts/common' $PROFILECONF || { 
     echo "" >> $PROFILECONF
-    echo 'if [ -d "$DIR/common" ] ; then' >> $PROFILECONF
-    echo '    PATH="$DIR/common:$PATH"' >> $PROFILECONF
-    echo 'fi' >> $PROFILECONF
+    echo "if [ -d "$DIR/common" ] ; then" >> $PROFILECONF
+    echo "    PATH=$DIR/common:'$PATH'" >> $PROFILECONF
+    echo "fi" >> $PROFILECONF
 }
 
 grep '$HOME/coding/scripts/pi' $PROFILECONF || { 
     echo "" >> $PROFILECONF
-    echo 'if [ -d "$DIR/pi" ] ; then' >> $PROFILECONF
-    echo '    PATH="$DIR/pi:$PATH"' >> $PROFILECONF
-    echo 'fi' >> $PROFILECONF
+    echo "if [ -d "$DIR/pi" ] ; then" >> $PROFILECONF
+    echo "    PATH=$DIR/pi:'$PATH'" >> $PROFILECONF
+    echo "fi" >> $PROFILECONF
 }
