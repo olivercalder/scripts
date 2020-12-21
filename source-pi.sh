8901aaf4b8d6c85ff5f9e3c70ea6efb180ae449e
Oliver Calder <olivercalder@protonmail.ch>
2020-12-21 02:14:05 -0600

    Fixed globbing.  Still need to fix a bunch of remote scripts which do not yet have proper globbing (and use bash instead of sh).

--- source-pi.sh
+++ source-pi.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-PROFILECONF=$HOME/.profile
+PROFILECONF="$HOME/.profile"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-grep "$DIR/common" $PROFILECONF > /dev/null || { 
-    echo "" >> $PROFILECONF
-    echo 'if [ -d '$DIR'/common ] ; then' >> $PROFILECONF
-    echo '    PATH="'$DIR'/common:$PATH"' >> $PROFILECONF
-    echo "fi" >> $PROFILECONF
+grep "$DIR/common" "$PROFILECONF" > /dev/null || { 
+    echo "" >> "$PROFILECONF"
+    echo 'if [ -d '$DIR'/common ] ; then' >> "$PROFILECONF"
+    echo '    PATH="'$DIR'/common:$PATH"' >> "$PROFILECONF"
+    echo "fi" >> "$PROFILECONF"
 }
 
-grep "$DIR/pi" $PROFILECONF > /dev/null || { 
-    echo "" >> $PROFILECONF
-    echo 'if [ -d '$DIR'/pi ] ; then' >> $PROFILECONF
-    echo '    PATH="'$DIR'/pi:$PATH"' >> $PROFILECONF
-    echo "fi" >> $PROFILECONF
+grep "$DIR/pi" "$PROFILECONF" > /dev/null || { 
+    echo "" >> "$PROFILECONF"
+    echo 'if [ -d '$DIR'/pi ] ; then' >> "$PROFILECONF"
+    echo '    PATH="'$DIR'/pi:$PATH"' >> "$PROFILECONF"
+    echo "fi" >> "$PROFILECONF"
 }
